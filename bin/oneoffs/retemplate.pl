025743bee9257b653cd3cede94acbe714dc639e6
Ted Timmons <ted-github@perljam.net>
2010-01-04 02:07:37 -0800

    add retemplate.pl

---
+++ bin/oneoffs/retemplate.pl
@@ -0,0 +1,123 @@
+#!/usr/bin/perl
+
+###############################################################################
+#
+# Copyright (c) 2009 Ted Timmons  <ted-bacon@perljam.net>
+# This software is released under the MIT license, cited below.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+###############################################################################
+
+
+use strict;
+use Data::Dumper;
+use Encode;
+use Getopt::Long;
+use lib '/home/tedt/git/wikibacon/';
+use TedderBot;
+
+use constant WIKI_LOGTIME => '{{subst:CURRENTYEAR}}-{{subst:CURRENTMONTH}}-     {{subst:CURRENTDAY2}} {{subst:CURRENTTIME}}';
+
+# If true, run through the process, but don't acutally output to Wikipedia.
+my $NOPOST = 0; 
+
+# Output to the debug location, not the ACTUAL location. Might also cause
+# messages to STDOUT/STDERR.
+my $DEBUG = 0;
+
+# Log lines
+my $LOG = '';
+
+GetOptions ("nopost"    => \$NOPOST,
+            "debug"     => \$DEBUG);
+
+
+my $tb = TedderBot->new( userfile => '/home/tedt/.wiki-userinfo', debug => 0 );
+my $mw = $tb->getMWAPI();
+
+unless($tb->okayToRun()) {
+  die "we are not approved to run. outta here.";
+}
+
+my $backlist = $mw->list ( { action => 'query',
+  list => 'backlinks',
+  bltitle => 'Template:Space telescopes',
+  blnamespace => '0',
+  bllimit => '500',
+  blredirect => '500',
+  #ucprop => 'ids|title|timestamp',
+  #ucdir => 'newer',  },
+  { max => 200, }
+  # not using a hook, we want the raw list
+  #hook => \&print_articles
+} ) || die $mw->{error}->{code} . ': ' . $mw->{error}->{details};
+
+my $emblist = $mw->list ( { action => 'query',
+  list => 'embeddedin',
+  eititle => 'Template:Space telescopes',
+  eilimit => '500',
+  #ucprop => 'ids|title|timestamp',
+  { max => 2, }
+  #hook => &process_article
+ } ) || die $mw->{error}->{code} . ': ' . $mw->{error}->{details};
+
+my %seen;
+my $count = 0;
+foreach my $entry (@$backlist, @$emblist) {
+  my $title = $entry->{title};
+  my $dtitle = decode_utf8($title);
+
+  next if $seen{$title}++;
+
+  my $page = $mw->get_page( { title => $title } );
+  my $c = $page->{'*'};
+
+  if ($c =~ /{{(no)?bots/) {
+     _debug("'''skipping [[$title]], bot exclusion.'''\n");
+     next;
+  }
+
+  (my $new_c = $c) =~ s/{{Space telescopes/{{Space observatories/g;
+  if ($new_c eq $c) {
+     _debug(":Couldn't change [[$title]], didn't match regex.\n");
+  } elsif (! $NOPOST) {
+
+    # this is a safety trigger to make sure we don't actually edit
+    # real articles without approval. Remove before flight.
+    print "updating $title\n"; exit;
+
+
+    $tb->replacePage($title, $new_c, "replace [[:Template:Space telescopes]] with [[:Template:Space observatories]] (bot edit)");
+    _debug(":updated [[$title]]\n");
+  }
+
+  #print "done.";
+
+  #print "\n";
+}
+
+$tb->appendPage('User:TedderBot/TranscludeReplace/log', $LOG, WIKI_LOGTIME, "update TranscludeReplace log (bot edit)");
+
+_debug(":updated pages: $count\n");
+
+exit;
+
+sub _debug {
+  $LOG .= join('', @_);
+}
