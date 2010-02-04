5dbbd3de6cf5968000355614207ad385070abea4
Ted Timmons <ted-github@perljam.net>
2010-02-04 01:47:27 -0800

    the drugs are telling me to steop for the evening

--- TedderBot/OregonArticles.pm
+++ TedderBot/OregonArticles.pm
@@ -26,7 +26,8 @@ package TedderBot::OregonArticles;
 
 use strict;
 use Data::Dumper; # TODO: probably temporary for debugging/implementation
-use Time::ParseDate;
+use Time::ParseDate; #parsedate
+use Time::CTime; # strftime
 use MediaWiki::API;
 use Storable;
 use TedderBot;
@@ -34,6 +35,7 @@ use TedderBot;
 # Inherit from TedderBot
 our @ISA = 'TedderBot';
 
+use constant EDIT_CUTOFF => parsedate("-90 days");
 
 # Is the passed title part of the WikiProject Oregon constellation of pages?
 #
@@ -114,23 +116,45 @@ sub evalUserContribs {
     list => 'usercontribs',
     #ucuser => $user,
     ucuser => 'User:' . ucfirst(lc $user),
-    uclimit =>'5',
+    uclimit =>'500',
 
     { max => 50000 }
   );
-print Dumper(\%param);
+
+#print Dumper(\%param); 
+my $ret = {};
 
 
   do {
     my $aret = $mw->api( \%param )
       || die $mw->{error}->{code} . ': ' . $mw->{error}->{details};
-print Dumper($aret); exit;
+    foreach my $contrib (@{$aret->{query}{usercontribs}}) {
+      my $time = parsedate($contrib->{timestamp});
+      my $month = strftime('%Y%m', gmtime($time));
+      my $title = $contrib->{title};
+    
+      # most recent edit?
+      $ret->{max} = $time > $ret->{max} ? $time : $ret->{max};
+      # edits for our time period
+      if ($time > EDIT_CUTOFF) { $ret->{edit}++; }
+    
+      # Okay, do some of the same things for our OREGON articles.
+      if ($self->isOregonArticle($title)) {
+        $ret->{ORmax} = $time > $ret->{ORmax} ? $time : $ret->{ORmax};
+        if ($time > EDIT_CUTOFF) { $ret->{ORedit}++; }
+      }
+
+      #print "$time / $month / $title\n";
+      #print Dumper($contrib);
+      #last;
+    } 
 
     # pick off our continue parameter.
-    $param{uccontinue} = $aret->{'query-continue'}{links}{plcontinue};
-#print Dumper($aret); exit;
+    #print Dumper($ret->{"query-continue"}); exit;
+    #$param{uccontinue} = $aret->{'query-continue'}{links}{plcontinue};
+    #print Dumper($aret); exit;
 
-    # Should only have one pageid there, so we'll just shift off the first one.
+        # Should only have one pageid there, so we'll just shift off the first one.
     my @id = keys %{$aret->{query}{pages}};
     #my $alist = $aret->{query}{pages}{$id[0]}{links};
 #print "alist: ", Dumper($alist); exit;
@@ -139,8 +163,11 @@ print Dumper($aret); exit;
       #my $title = $entry->{title};
       #$list{$title}++;
     #}
+    print Dumper($ret->{"query-continue"}); exit;
+    $param{uccontinue} = $aret->{'query-continue'}{links}{plcontinue};
 
   } while ($param{uccontinue});
+print Dumper($ret);
 
 #print "output: ", Dumper($alist), "\n";
 #print "contribs: ", scalar @list, "\n";
